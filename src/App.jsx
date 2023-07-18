d8f4721137be4c29200e82c0044bf818f43a0c11
Sulejman Hoxha <sulejman.hoxha2001@hotmail.com>
2023-07-18 04:20:13 +0200

    add Routes and created blank pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import { DataProvider } from "./context/DataContext.jsx";
+import { Route, Routes } from "react-router-dom";
 
+import AddProduct from "./pages/AddProduct.jsx";
+import EditProduct from "./pages/EditProduct.jsx";
+import Home from "./pages/Home.jsx";
+import Missing from "./pages/Missing.jsx";
+import ViewProduct from "./pages/ViewProduct.jsx";
 function App() {
 	return (
 		<>
 			<DataProvider>
-				<p>Hello World</p>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/product/:id" element={<ViewProduct />} />
+					<Route path="/product/edit/:id" element={<EditProduct />} />
+					<Route path="/product/add" element={<AddProduct />} />
+					<Route path="*" element={<Missing />} />
+				</Routes>
 			</DataProvider>
 		</>
 	);
