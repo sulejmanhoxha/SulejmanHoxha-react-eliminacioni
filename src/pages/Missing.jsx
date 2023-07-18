968adf3032bad7d436c3fe5046460a2cbd32be7d
Sulejman Hoxha <sulejman.hoxha2001@hotmail.com>
2023-07-18 04:50:58 +0200

    add Missing page

--- src/pages/Missing.jsx
+++ src/pages/Missing.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Missing = () => {
-	return <div>Missing</div>;
+	return (
+		<div className="d-flex min-vh-100 align-items-center justify-content-center">
+			<div>
+				<div className="text-center">
+					<span className="display-1 mb-4">404</span>
+					<div className="mb-4 lead">
+						The page you are looking for was not found.
+					</div>
+					<Link to="/" className="btn btn-link">
+						Back to Home
+					</Link>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Missing;
