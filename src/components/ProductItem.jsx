5f3cb0922919df8d9b38acd996567433fe8ed4b9
Sulejman Hoxha <sulejman.hoxha2001@hotmail.com>
2023-07-20 19:35:07 +0200

    update ProductItem links and info it displays

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,12 +1,16 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useContext } from "react";
+import { Link, useParams } from "react-router-dom";
+
+import DataContext from "../context/DataContext";
 
 const ProductItem = ({ item }) => {
+	const { id } = useParams();
+
 	return (
 		<div className="col my-3">
 			<div className="card">
 				<img
-					src={item.thumbnail} // Use the item's thumbnail URL
+					src={item.thumbnail}
 					style={{ height: "200px" }}
 					className="card-img-top"
 				/>
@@ -15,30 +19,10 @@ const ProductItem = ({ item }) => {
 						<h5 className="card-title text-primary">
 							{item.title}
 						</h5>
-						{/* Use the item's title */}
-						<div className="card-subtitle text-secondary mb-2">
-							{item.brand} {/* Use the item's brand */}
-						</div>
-						<p className="card-text">
-							{item.description.length <= 55
-								? item.description
-								: `${item.description.slice(0, 55)}...`}
-						</p>
-						{/* Use the item's description */}
 					</div>
 					<ul className="list-group list-group-flush">
 						<li className="list-group-item text-success">
-							Price: {item.price} $ {/* Use the item's price */}
-						</li>
-						<li className="list-group-item">
-							Discount Percentage: {item.discountPercentage}
-							{/* Use the item's discountPercentage */}
-						</li>
-						<li className="list-group-item">Stock: {item.stock}</li>
-						{/* Use the item's stock */}
-						<li className="list-group-item">
-							Category: {item.category}
-							{/* Use the item's category */}
+							Price: {item.price} $
 						</li>
 					</ul>
 					<div className="card-footer d-flex justify-content-center gap-3">
@@ -49,7 +33,10 @@ const ProductItem = ({ item }) => {
 							View Details
 						</Link>
 
-						<Link className="btn btn-success btn-sm text-nowrap">
+						<Link
+							to={`/product/edit/${item.id}`}
+							className="btn btn-success btn-sm text-nowrap"
+						>
 							Edit Product
 						</Link>
 						<Link className="btn btn-danger btn-sm text-nowrap">
